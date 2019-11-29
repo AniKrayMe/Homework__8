f7e8f2b594852bb52430cb00e31a7e52cb78daca
serian <sseriann@mail.ru>
2019-11-29 16:11:56 +0400

    8_3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please print color ");
-        String color = scanner.next();
+        System.out.println("Please print index ");
+        int index = scanner.nextInt();
         ArrayList<String> colors = new ArrayList<>();
-        colors.add(0, color);
         colors.add("Green");
         colors.add("Black");
         colors.add("Blue");
         colors.add("Yellow");
         colors.add("Red");
+        colors.remove(index);
         for (int i = 0; i < colors.size(); i++) {
             System.out.print(colors.get(i) + ", ");
         }
