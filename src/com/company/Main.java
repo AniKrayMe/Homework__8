927ac2728085866278279df6f6bbf742b119bd39
serian <sseriann@mail.ru>
2019-11-29 15:59:32 +0400

    8_2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please print color ");
+        String color = scanner.next();
         ArrayList<String> colors = new ArrayList<>();
+        colors.add(0, color);
         colors.add("Green");
         colors.add("Black");
         colors.add("Blue");
