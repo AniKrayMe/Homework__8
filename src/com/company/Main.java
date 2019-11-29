edee5b0de353d41e2696e5a39b0ce60b330610da
serian <sseriann@mail.ru>
2019-11-29 16:13:41 +0400

    8_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,17 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please print index ");
-        int index = scanner.nextInt();
         ArrayList<String> colors = new ArrayList<>();
         colors.add("Green");
         colors.add("Black");
         colors.add("Blue");
         colors.add("Yellow");
         colors.add("Red");
-        colors.remove(index);
+        colors.remove(2);
         for (int i = 0; i < colors.size(); i++) {
             System.out.print(colors.get(i) + ", ");
         }
