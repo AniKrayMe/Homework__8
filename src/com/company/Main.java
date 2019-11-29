314807497137f6a4fe2c0d31ca91efa51f0fc9a6
serian <sseriann@mail.ru>
2019-11-29 16:33:13 +0400

    8_7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,13 +12,28 @@ public class Main {
         colors.add("Blue");
         colors.add("Yellow");
         colors.add("Red");
+        System.out.print("colors = ");
         for (int i = 0; i < colors.size(); i++) {
             System.out.print(colors.get(i) + ", ");
         }
-        Collections.reverse(colors);
-        System.out.println("\n" + "Reserved array ");
-        for (int i = 0; i < colors.size(); i++) {
-            System.out.print(colors.get(i) + ", ");
+        System.out.println();
+        ArrayList<String> colors2 = new ArrayList<>();
+        colors2.add("Silver");
+        colors2.add("Fuchsia");
+        colors2.add("Pink");
+        colors2.add("Lime");
+        colors2.add("Aqua");
+        System.out.print("colors2 = ");
+        for (int i = 0; i < colors2.size(); i++) {
+            System.out.print(colors2.get(i) + ", ");
+        }
+        System.out.println();
+        ArrayList<String> joined = new ArrayList<>();
+        joined.addAll(colors);
+        joined.addAll(colors2);
+        System.out.print("colors + colors2 = ");
+        for (int i = 0; i <joined.size(); i++) {
+            System.out.print(joined.get(i) + ", ");
         }
     }
 }
